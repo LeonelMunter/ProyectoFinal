3b9f0bc29d66261e6e4aa91e758555d226163d90
Isaias Ramos Lopez <isaias.ramos.27@unsch.edu.pe>
2023-03-13 18:10:15 -0500

    Enabling the light and dark thema

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,34 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { useState, useEffect } from "react";
+import { changeTheme } from "../app/themeSlice";
 
 const Navbar = () => {
+  const themeValue = useSelector((state) => state.theme.theme);
+
+  const [localThemeValue, setLocalThemeValue] = useState(themeValue);
+  const htmlDoc = document.documentElement;
+
+  const dispatch = useDispatch();
+
+  const handleThemeChange = (event) => {
+    event.preventDefault();
+    setLocalThemeValue(localThemeValue === "light" ? "dark" : "light");
+    dispatch(changeTheme(localThemeValue === "light" ? "dark" : "light"));
+  };
+
+  useEffect(() => {
+    localThemeValue === "light" ? htmlDoc.classList.remove("dark") : htmlDoc.classList.add("dark");
+  }, [localThemeValue]);
+
   return (
     <>
-      <div>Navbar</div>
+      <div className="flex dark:bg-gray-900">
+        <button className="text-2xl pb-8 font-bold dark:text-gray-400" onClick={(event) => handleThemeChange(event)}>
+          {localThemeValue === "light" ? "LIGTH THEME" : "DARK THEME"}
+        </button>
+      </div>
       <Outlet />
     </>
   );
--- client/src/pages/Landing.jsx
+++ client/src/pages/Landing.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <div className="min-h-screen flex flex-col justify-center items-center transition duration-200 dark:bg-gray-900">
+      <span className="text-2xl font-bold dark:text-gray-400">Landing</span>
+    </div>
+  );
 };
 
 export default Landing;
