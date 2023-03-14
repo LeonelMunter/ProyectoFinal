62ad352107e0a1a1b7b0af10f412b18c01ddabb8
Isaias Ramos Lopez <isaias.ramos.27@unsch.edu.pe>
2023-03-13 19:15:45 -0500

    multilanguage enabled

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,9 +1,10 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
-
 import themeReducer from "./themeSlice";
+import languageReducer from "./languageSlice";
 
 const rootReducer = combineReducers({
   theme: themeReducer,
+  language: languageReducer,
 });
 
 export const store = configureStore({
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import i18n from "./i18n";
+import { I18nextProvider } from "react-i18next";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./app/store";
@@ -7,6 +9,8 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
   </Provider>
 );
