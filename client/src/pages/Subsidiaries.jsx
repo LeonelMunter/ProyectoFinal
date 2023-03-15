29e498de25d515d17602a1ba19f8fb90daf5b8be
Isaias Ramos Lopez <isaias.ramos.27@unsch.edu.pe>
2023-03-15 12:34:34 -0500

    add two page,

---
+++ client/src/pages/Subsidiaries.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Subsidiaries = () => {
+  return (
+    <div className="min-h-screen dark:bg-gray-900">
+      <p className="text-3xl p-5 font-bold dark:text-gray-300">Subsidiaries</p>
+    </div>
+  );
+};
+
+export default Subsidiaries;
